9dc5bcdab42dcb24ffe22f282d88c253b00c0924
Zabrina Osborne <Laydiebri24@hotmail.com>
2021-03-19 14:25:29 +0000

    First commit, adding initial files

---
+++ script22.js
@@ -0,0 +1,3 @@
+function msg() {
+    document.write("Not that button!")
+}
---
+++ script4.js
@@ -0,0 +1,4 @@
+function change() {
+    var element = document.getElementById("my_text");
+    element.style['color']= "red";
+}
